f52550dddf8b576ec4fa590498a296594ffba84c
Diego Cali <diegocali123@gmail.com>
2025-01-25 11:58:37 -0600

    mod: example_template.gd

--- src/scripts/example_scene.gd
+++ src/scripts/example_scene.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
-
+@export var variable_visual = 2
+signal popped
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,3 +10,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func mouse_entered_body():
+	popped.emit(true)
