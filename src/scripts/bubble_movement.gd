4011fcd80f02ea3951fb7d1a9b4a356f7478fbee
Diego Cali <diegocali123@gmail.com>
2025-01-26 10:53:46 -0600

    added: bubble animation

--- src/scripts/bubble_movement.gd
+++ src/scripts/bubble_movement.gd
@@ -1,10 +1,14 @@
 extends Node2D
 
-var velocity = Vector2(50, 50)
-
+@export var vx = 50
+@export var vy = 50
+var velocity = Vector2(0,0)
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	velocity = Vector2(vx, vy)
+	$AnimatedSprite2D.play("good_static")
 	$Area2D.connect("input_event", self._on_area2d_input_event)
+	print(velocity)
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
