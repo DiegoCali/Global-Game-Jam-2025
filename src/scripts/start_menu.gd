1beaac947b71cc4d73ba4da28b49b3f70441d5b5
Javier Avila <floresjavier092@gmail.com>
2025-01-26 11:44:34 -0600

    Add animation hud

--- src/scripts/start_menu.gd
+++ src/scripts/start_menu.gd
@@ -4,6 +4,7 @@ signal game_start
 signal restart
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	
 	pass # Replace with function body.
 
 
@@ -16,4 +17,3 @@ func _process(_delta: float) -> void:
 func _on_button_pressed() -> void:
 	game_start.emit()
 	self.hide()
-	get_tree().change_scene_to_file("res://scenes/hud.tscn")
