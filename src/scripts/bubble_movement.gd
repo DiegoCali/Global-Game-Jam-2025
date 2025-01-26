54868b469619e572b963c862c80b58a475bbb521
Javier Avila <floresjavier092@gmail.com>
2025-01-26 12:08:30 -0600

    update main

--- src/scripts/bubble_movement.gd
+++ src/scripts/bubble_movement.gd
@@ -1,22 +1,10 @@
 extends Node2D
 
-@export var vx = 50
-@export var vy = 50
-var velocity = Vector2(0,0)
-var rng = RandomNumberGenerator.new()
-var IamBad = false
-signal popped
+var velocity = Vector2(50, 50)
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	velocity = Vector2(vx, vy)
-	IamBad = rng.randi() % 2 == 0
-	if not IamBad:
-		$AnimatedSprite2D.play("good_static")
-	else:
-		$AnimatedSprite2D.play("bad_static")
-		$AnimatedSprite2D.set_scale(Vector2(0.32, 0.32))
 	$Area2D.connect("input_event", self._on_area2d_input_event)
-	print(velocity)
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -24,14 +12,4 @@ func _process(delta: float) -> void:
 
 func _on_area2d_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.pressed:
-		if not IamBad:
-			$AnimatedSprite2D.play("pop_good")
-			$AnimatedSprite2D.set_scale(Vector2(0.3, 0.3))
-		else:
-			$AnimatedSprite2D.play("pop_bad")
-
-
-func _on_animated_sprite_2d_animation_finished() -> void:
-	print("Goodbye")
-	popped.emit(IamBad)
-	queue_free()
+		queue_free()
